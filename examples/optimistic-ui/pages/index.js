95c5401d24319ad1487d248424f8b3aadd858b48
Reza Babaei <reza.tizro@gmail.com>
2021-07-06 01:45:21 +0300

    some consistency

--- examples/optimistic-ui/pages/index.js
+++ examples/optimistic-ui/pages/index.js
@@ -2,12 +2,13 @@ import useSWR from "swr"
 // https://github.com/vercel/swr/discussions/582
 
 export default function Home() {
-  const { data } = useSWR("https://my.backend/posts?take=1")
+  const { data, isValidating } = useSWR("https://my.backend/posts?take=1")
 
   console.log('data', data)
   return (
     <div>
-    {data.map(p => <div key={p.id}>{p.title}</div>)}
+    {isValidating && <div>loading...</div>}
+    {!isValidating && data?.map(p => <div key={p.id}>{p.title}</div>)}
     </div>
   )
 }
